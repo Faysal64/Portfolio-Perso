9b6e3cbb309891f3ecfe69753d2eddaccb116da3
Faysal64 <fdriouach10@gmail.com>
2025-05-14 15:17:13 +0200

    Ajout des compétences et modification de police de texte

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,23 +10,31 @@ function MainContent() {
     <main>
 
       {/* --------- HERO --------- */}
-      <section className="main-content">
-        <div className="text-content">
-          <h1>
-            <span className="first-name">Faysal</span> <span className="last-name">Driouach</span>
-          </h1>
-          <h2>Développeur Front-end.</h2>
-          <p>
-            Autodidacte passionné de développement web, UI/UX et accessibilité. 
-          </p>
-          <div className="buttons">
-            <a href="#contact" className="btn primary">ME CONTACTER</a>
-            <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="btn secondary">GITHUB</a>
-          </div>
-        </div>
+      <section className="hero-section">  {/* PAS de style ici */}
+  <div className="main-content">
 
-        <div className="image-content">
-          <img src={Profil} alt="Faysal Driouach" />
+          <div className="text-content">
+            <h1>
+              <span className="intro">Hello !</span><br />
+              <span className="first-name">Moi c’est</span> <span className="last-name">Faysal.</span>
+            </h1>
+            <h2>Développeur Front-end</h2>
+            <p>
+              Ensemble, réalisons des projets innovants. <br />
+              À distance ou sur site, je suis prêt à relever de nouveaux défis.
+            </p>
+            <div className="buttons">
+              <a href="#contact" className="btn primary">Contactez-moi</a>
+              <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="btn secondary">Voir sur GitHub</a>
+              <a href="/cv.pdf" target="_blank" rel="noopener noreferrer" className="btn download">
+                <span className="icon">⬇</span> Télécharger mon CV
+              </a>
+            </div>
+          </div>
+
+          <div className="image-content">
+            <img src={Profil} alt="Faysal Driouach" />
+          </div>
         </div>
       </section>
 
